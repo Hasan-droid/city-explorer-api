4eeda9d30bc9840ff3066bf7cbea33189278c0bb
hasan-droid <hasan-net2010@hotmail.com>
2021-07-09 18:59:55 +0300

    cash added

--- modules/Movie.js
+++ modules/Movie.js
@@ -1,13 +1,24 @@
 const axios = require('axios');
-
+const memory={};
 const movie= (req , res)=> {
     let query=req.query.city;
-let url= `https://api.themoviedb.org/3/search/movie?api_key=${process.env.themoviedb_API_KEY}&query=${query}`
-const getMoviesAPI=axios.get(url).then(response=>{
-    const movies=response.data.results;
-    const findData=movies.map(item=> new renderMovies(item))
-    res.send(findData);
-}).catch(error=>res.send(erorr.message));
+    let memoryKey=query;
+    if(memoryKey in memory)
+    {   console.log('cache hit')
+        res.send(memory[memoryKey])
+    }
+    else
+    {
+        let url= `https://api.themoviedb.org/3/search/movie?api_key=${process.env.themoviedb_API_KEY}&query=${query}`
+        const getMoviesAPI=axios.get(url).then(response=>{
+            const movies=response.data.results;
+            const findData=movies.map(item=> new renderMovies(item))
+            memory[memoryKey]=findData;
+            console.log('cache missed')
+            res.send(findData);
+        }).catch(error=>res.send(erorr.message));
+    }
+
 
 
 }
--- modules/Weather.js
+++ modules/Weather.js
@@ -1,19 +1,29 @@
 
 const axios = require('axios');
-
+const memory={};
 const weather = (req, res) => {
     let lat = req.query.lat;
     let lon = req.query.lon;
+    let memoryKey=`${lat}-${lon}`;
+    if(memoryKey in memory)
+    {   console.log('cache hit')
+        res.send(memory[memoryKey])
+    }
+    else
+    {
+        let url = `${process.env.weathbit_DOMAIN}?key=${process.env.weathbit_API_KEY}&lat=${lat}&lon=${lon}`
+        const getWeathebitData = axios.get(url).then(response => {
+            const weather = response.data;
+            const findData = weather.data.map(item => new forecast(item))
+            memory[memoryKey]=findData;
+            console.log('cache missed')
+            res.send(findData);
+            // console.log(weather);
+    
+        }).catch(error => res.send(error.message));
+    }
 
-
-    let url = `${process.env.weathbit_DOMAIN}?key=${process.env.weathbit_API_KEY}&lat=${lat}&lon=${lon}`
-    const getWeathebitData = axios.get(url).then(response => {
-        const weather = response.data;
-        const findData = weather.data.map(item => new forecast(item))
-        res.send(findData);
-        console.log(weather);
-
-    }).catch(error => res.send(error.message));
+   
 
 }
 
